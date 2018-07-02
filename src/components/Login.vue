f994ab80c01d23e00e433e0a5d6e4b5403104e2c
Eduard Korolev <karevseo@mail.ru>
2018-07-02 23:09:42 +0300

    Добавляет авторизацию

---
+++ src/components/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <form class="login" @submit.prevent="login">
+      <h1>Sign in</h1>
+        <label>User name</label>
+        <input required v-model="username" type="text" placeholder=""/>
+        <label>Password</label>
+        <input required v-model="password" type="password" placeholder=""/>
+        <hr/>
+        <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<style>
+  .login {
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    padding: 10px;
+  }
+</style>
+
+<script>
+import axios from 'axios';
+import FormData from 'form-data';
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+        username: '',
+        password: '',
+    }
+  },
+  methods: {
+    login: function () {
+      var { username, password } = this;
+      axios(
+        {
+          method: 'post',
+          url: 'http://laravel.test/api/login',
+          data: {
+            email: username,
+            password: password
+          },
+          config: { headers: {'Content-Type': 'multipart/form-data' }}
+        }
+      )
+      .then(responce => {
+          const token = responce.data.success.token;
+          localStorage.setItem('token', token)
+          this.$router.push('/')
+          console.log(responce.data.success.token)
+      })
+      .catch(e => {
+          this.$router.push('/login')
+          this.errors.push(e)
+      })
+    }
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (!localStorage.getItem('token')) {
+    next()
+    return
+  }
+  next('/')
+}
+
+const ifAuthenticated = (to, from, next) => {
+  if (localStorage.getItem('token')) {
+    next()
+    return
+  }
+  next('/login')
+}
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+      beforeEnter: ifAuthenticated,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      beforeEnter: ifNotAuthenticated,
+    }
+  ]
+})
